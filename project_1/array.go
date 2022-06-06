b605c706a25863895a58e6dd1f6327504d333870
MIrkomil <m.saitov@uzinfocom.uz>
2022-06-06 16:06:38 +0500

    ready array

--- project_1/array.go
+++ project_1/array.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	//array()
 	//ex1()
-	ex2()
+	//ex2()
+	//ex3()
+	//ex4()
+	ex5()
 }
 
 func array() {
@@ -67,4 +70,53 @@ func ex2() {
 		fmt.Print(nums[j], " ")
 	}
 	fmt.Fprintf(os.Stdout, "\n%d", j)
+}
+
+func ex3() {
+	x := [...]float32{2.1, 3.2, 4.7}
+	fmt.Println(x)
+
+	var total float32
+	for _, val := range x {
+		total += val
+	}
+	fmt.Println(total)
+}
+
+func ex4() {
+	type Size int
+
+	const (
+		EX Size = iota
+		LG
+		MD
+		SM
+	)
+	sz := [...]string{EX: "Extra Large", LG: "Large", MD: "Medium", SM: "Small"}
+	fmt.Println(MD, sz[MD])
+	fmt.Println(sz)
+
+	x := [...]int{3: 10, 20}
+	fmt.Println(x)
+}
+
+func ex5() {
+	a := [2][3]int{{2, 4, 6}, {4, 6, 8}}
+	fmt.Printf("a=%v type=%T len=%d\n", a, a, len(a))
+
+	var b [2][3]int
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			b[i][j] = (j + j + 1) * 2
+		}
+	}
+
+	fmt.Printf("a=%v type=%T len=%d\n", b, b, len(b))
+
+	a[0][1] = 5
+	a[1][1] = 10
+
+	fmt.Printf("a=%v type=%T len=%d\n", a, a, len(a))
+
 }
