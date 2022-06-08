a213bce2b8b4412c554917d390d8799fa457d8f6
MirkomilSaitov2204 <mike_saitov@mail.ru>
2022-06-08 10:39:26 +0500

    fixing code

--- project_1/Unit1_Exercises.go
+++ project_1/Unit1_Exercises.go
@@ -8,7 +8,8 @@ func main() {
 	//third()
 	//fourth()
 	//fifth()
-	findArea()
+	//findArea()
+	findPopulation()
 }
 
 func first() {
@@ -74,5 +75,42 @@ func findArea() {
 
 	fmt.Println(perimetr)
 	fmt.Println(area)
+
+}
+
+func findPopulation() {
+	//■ One birth every 7 seconds
+	//■ One death every 13 seconds
+	//■ One new immigrant every 45 seconds
+
+	population := 312032486
+
+	years := 5
+	days := 365
+	hours := 24
+	minuts := 60
+
+	secondsInOneYear := (60 * minuts) * hours * days
+
+	fmt.Printf("Seconds in one years  %v\n\n", secondsInOneYear)
+
+	birthsInYear := secondsInOneYear / 7
+	deathsInYear := secondsInOneYear / 13
+	immigrantsInYear := secondsInOneYear / 45
+
+	fmt.Printf("Birth rate in a year  %v\n\n", birthsInYear)
+	fmt.Printf("Death rate in a year  %v\n\n", deathsInYear)
+	fmt.Printf("Immigrant rate in a year  %v\n\n", immigrantsInYear)
+
+	TotalAdditionInYear := birthsInYear + immigrantsInYear - deathsInYear
+
+	fmt.Printf("Total Addition in a year  %v\n\n", TotalAdditionInYear)
+	fmt.Println("======================")
+	for i := 1; i <= years; i++ {
+		population += TotalAdditionInYear
+		fmt.Println("======================")
+		fmt.Printf("Population in one Year  %v\n\n", population)
+
+	}
 
 }
