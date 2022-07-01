6c26aee5d88c9d418f841c530a19452b080d20c6
MirkomilSaitov2204 <mike_saitov@mail.ru>
2022-07-01 11:55:20 +0500

    fix art

--- project_1/structs.go
+++ project_1/structs.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //type player struct {
 //	name, sport string
 //	age         int
@@ -11,7 +13,47 @@ package main
 //	DOB          date.Date
 //}
 
+//type book struct {
+//	name   string
+//	author string
+//}
+
+type player struct {
+	name, sport string
+	age         int
+	info        generalInfo
+}
+type generalInfo struct {
+	country, color string
+}
+
 func main() {
+
+	pl := player{"21312", "12312312", 44, generalInfo{"2333", "55555"}}
+	fmt.Println(pl)
+
+	//b1 := book{"Go Programming", "Author 1"}
+	//b2 := book{"Go Programming", "Author 1"}
+	//b3 := book{"C++ Programming", "Author 2"}
+	//
+	//fmt.Printf("LOC1 - %v\n", b1)
+	//
+	//fmt.Println(compare1(b1, b2))
+	//fmt.Println(compare1(b1, b3))
+	//
+	//fmt.Println(compare2(&b1, &b2))
+	//fmt.Printf("LOC3 - %v\n\n", b1)
+	//
+	//p1 := &book{"Go Web Programming", "Author 3"}
+	//
+	//p2 := new(book)
+	//*p2 = book{"Go Web Programming", "Author 3"}
+	//
+	//fmt.Printf("LOC4 - &p1=%v *p1=%v\n", &p1, *p1)
+	//
+	//fmt.Println(compare2(p1, p2))
+	//fmt.Printf("LOC5 - &p1=%v *p1=%v\n", &p1, *p1)
+
 	//var emp employee
 	//
 	//emp.name = "Mike"
@@ -69,4 +111,38 @@ func main() {
 	//
 	//fmt.Println(player1)
 
+	//testSizeOfStruct()
+
 }
+
+//func compare1(b book, q book) bool {
+//	if b.name == q.name && b.author == q.author {
+//		b.name = "Java programming"
+//		return true
+//	}
+//	return false
+//}
+//
+//func compare2(b *book, q *book) bool {
+//	if b.name == q.name && b.author == q.author {
+//		b.name = "Python Programming"
+//		return true
+//	}
+//	return false
+//}
+
+//func testSizeOfStruct() {
+//	type values struct {
+//		val1 int32 // 4 bytes
+//		val2 int32 // 4 bytes
+//		val3 int64 // 8 bytes
+//		val4 int64 // 8 bytes
+//	}
+//
+//	x := values{10, 20, 30, 40}
+//	p := &x
+//	fmt.Println(unsafe.Sizeof(p), unsafe.Sizeof(*p), unsafe.Sizeof(x))
+//
+//	q := &values{11, 21, 31, 41}
+//	fmt.Println(unsafe.Sizeof(q), unsafe.Sizeof(*q))
+//}
