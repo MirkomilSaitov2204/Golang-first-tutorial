9f5db13d0405a78e8d483226c853872ffb47f4ba
MirkomilSaitov2204 <mike_saitov@mail.ru>
2022-07-07 12:16:55 +0500

    fix

--- project_1/packages/main.go
+++ project_1/packages/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"mirkomil/packages/shapes1"
+	"mirkomil/packages/shapes2"
+)
+
 func main() {
+
+}
+
+func testCase1() {
+	fmt.Println(shapes1.Area(shapes1.Length, shapes1.Width))
+	fmt.Println(shapes1.SquareArea(2))
+	fmt.Println(shapes2.Area(2, 3))
+	fmt.Println(shapes2.SquareArea(2))
 
 }
